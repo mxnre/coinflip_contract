f52c42d46b2884332408b5d559fee95e0b70fb68
mxnre <mnteristo@gmail.com>
2021-09-15 01:43:57 +0800

    Add migration

---
+++ migrations/2_deploy_coinflip.js
@@ -0,0 +1,13 @@
+const CoinFlip = artifacts.require('CoinFlip');
+
+module.exports = async function (deployer) {
+
+    await deployer.deploy(
+        CoinFlip,
+        "0xB1310c700699eC24D67beAc7164cea0792069080", // Deployed ULP Address
+        "0xC238001552195f0E159798CdA94DC22a71993bC9", // Deployed GBTS Address
+        "0xfe18A7673bAa26870B4f8df6a9cCE811E1f88e52", // // Deployed RNG Address
+    );
+
+    return;
+};
